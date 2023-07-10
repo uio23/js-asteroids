3611a09bac3484834c54ee13d8458281af181a04
uio23 <quantumTunnel56@icloud.com>
2023-07-10 14:38:20 +1200

    prep for beta deploy

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ io.on('connection', (socket) => {
 });
 
 
-server.listen(8080, () => {
-  console.log('listening on port 8080');
+server.listen(process.env.PORT || 8080, () => {
+  console.log('listening');
 });
